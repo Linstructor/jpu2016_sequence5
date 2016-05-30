1f0d89b54bc935ff17a8df914237ac0439e66d51
Linstructor <llaunch14@gmail.com>
2016-05-30 22:19:38 +0200

    création de mermaid avec image

--- src/jpu2016/nettle/world/element/mobile/Mermaid.java
+++ src/jpu2016/nettle/world/element/mobile/Mermaid.java
@@ -1,12 +1,25 @@
 package jpu2016.nettle.world.element.mobile;
 
-import jpu2016.nettle.world.element.ISprite;
+import jpu2016.nettle.world.element.Sprite;
+
+import java.awt.*;
 
 /**
  * Created by Tristan on 30/05/2016.
  */
 public class Mermaid extends Swimming {
-    public Mermaid(ISprite sprite) {
-        super(sprite);
+
+    private final Point position;
+
+    public Mermaid(Point position) {
+        super(new Sprite("~~", "mermaid.png"));
+        this.position = new Point();
+        this.position.setLocation(this.getPosition().x, this.getPosition().y);
+    }
+
+    private void saveLastPosition() {
+        if ((this.position.getX() != this.getPosition().getX()) || (this.position.getY() != this.getPosition().getY())) {
+            this.position.setLocation(this.getPosition().x, this.getPosition().y);
+        }
     }
 }
