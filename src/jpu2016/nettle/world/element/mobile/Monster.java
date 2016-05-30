4ff5adec919b9031928bc93eca8c783ab87b5902
Linstructor <llaunch14@gmail.com>
2016-05-30 22:30:07 +0200

    création de mermaid avec image

--- src/jpu2016/nettle/world/element/mobile/Monster.java
+++ src/jpu2016/nettle/world/element/mobile/Monster.java
@@ -4,11 +4,15 @@ import jpu2016.nettle.world.element.ISprite;
 
 public class Monster extends Mobile implements Animate {
 
+    Animate animate;
 
     public Monster(ISprite sprite) {
         super(sprite);
+        this.animate = animate;
     }
 
-    public void animate(){
+    @Override
+    public void animate() {
+
     }
 }
--- src/jpu2016/nettle/world/element/mobile/Swimming.java
+++ src/jpu2016/nettle/world/element/mobile/Swimming.java
@@ -1,13 +1,14 @@
 package jpu2016.nettle.world.element.mobile;
 
 import jpu2016.nettle.world.element.ISprite;
+import jpu2016.nettle.world.element.Sprite;
 
 /**
  * Created by Tristan on 30/05/2016.
  */
 public class Swimming extends Monster implements Animate{
 
-    public Swimming(ISprite sprite) {
+    public Swimming(Sprite sprite) {
         super(sprite);
     }
 
