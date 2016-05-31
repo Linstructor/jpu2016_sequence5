964dc05dca3fa8f7c221f38f8f457bad73c7675a
Linstructor <llaunch14@gmail.com>
2016-05-31 10:09:16 +0200

    Dragon et flying

--- src/jpu2016/nettle/world/element/mobile/Mermaid.java
+++ src/jpu2016/nettle/world/element/mobile/Mermaid.java
@@ -7,19 +7,5 @@ import java.awt.*;
 /**
  * Created by Tristan on 30/05/2016.
  */
-public class Mermaid extends Swimming {
-
-    private final Point position;
-
-    public Mermaid(Point position) {
-        super(new Sprite("~~", "mermaid.png"));
-        this.position = new Point();
-        this.position.setLocation(this.getPosition().x, this.getPosition().y);
-    }
-
-    private void saveLastPosition() {
-        if ((this.position.getX() != this.getPosition().getX()) || (this.position.getY() != this.getPosition().getY())) {
-            this.position.setLocation(this.getPosition().x, this.getPosition().y);
-        }
-    }
+public class Mermaid {
 }
