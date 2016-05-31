a58b314519672bc1d01210447e0e6717af30b793
Linstructor <llaunch14@gmail.com>
2016-05-31 10:08:47 +0200

    Dragon et flying

--- src/jpu2016/nettle/world/element/mobile/Wizard.java
+++ src/jpu2016/nettle/world/element/mobile/Wizard.java
@@ -5,8 +5,5 @@ import jpu2016.nettle.world.element.ISprite;
 /**
  * Created by Tristan on 30/05/2016.
  */
-public class Wizard extends Shooting {
-    public Wizard(ISprite sprite) {
-        super(sprite);
-    }
+public class Wizard {
 }
